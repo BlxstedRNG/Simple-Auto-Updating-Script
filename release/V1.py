480169962328cab734ee76235527d39aba6e6ddc
max <148343900+BlxstedRNG@users.noreply.github.com>
2023-11-24 19:29:55 +0000

    V1.py

--- release/V1.py
+++ release/V1.py
@@ -1,5 +1,5 @@
 # Your main Python script on the server (e.g., server_script.py)
-version = "1.1"  # Your current version
+version = "1.0"  # Your current version
 
 def main():
     print("Hello, Server!")
