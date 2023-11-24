720ea99c88e261d8bd1e79424292d297197a064f
max <148343900+BlxstedRNG@users.noreply.github.com>
2023-11-24 19:26:20 +0000

    Update V1.py

--- release/V1.py
+++ release/V1.py
@@ -1,2 +1,9 @@
-import tkinter as tk
-from tkinter import tkinter
+# Your main Python script on the server (e.g., server_script.py)
+version = "1.1"  # Your current version
+
+def main():
+    print("Hello, Server!")
+
+if __name__ == "__main__":
+    main()
+
